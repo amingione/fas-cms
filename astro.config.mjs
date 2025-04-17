deeff9af1cb901543c5a472bda179ab5e5e0e6c0
Amber Mingione <ambermingione@gmail.com>
2025-04-16 20:30:43 -0400

    feat: integrate Clerk for authentication and enhance account dashboard functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+
 
 export default defineConfig({
+  integrations: [clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
   output: 'server',
   adapter: netlify(),
   integrations: [react(), tailwind()],
